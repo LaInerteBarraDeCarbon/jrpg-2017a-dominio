fe5e0a1e0c4ae8b002c6d64e64d1f06f9875d397
Unknown <o_frutos@yahoo.com>
2017-04-20 01:00:14 -0300

    Documentación Peleable, Personaje y MyRandom

--- src/main/java/dominio/MyRandom.java
+++ src/main/java/dominio/MyRandom.java
@@ -1,12 +1,27 @@
 package dominio;
 
+/**
+ * La clase MyRandom se utiliza para calcular elementos random, sea para las
+ * batallas como para la dificultad. <br>
+ */
 public class MyRandom {
 
+	/**
+	 * Devuelve un número random. <br>
+	 * 
+	 * @return Número. <br>
+	 */
 	public static double nextDouble() {
 		return 0.49;
 	}
+
+	/**
+	 * Retorna el valor anterior al dado. <br>
+	 * 
+	 * @param val
+	 * @return Valor. <br>
+	 */
 	public static int nextInt(int val) {
 		return val - 1;
 	}
-	
 }
