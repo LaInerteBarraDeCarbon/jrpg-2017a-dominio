312582ba81c18d8566f0f3e41c97ccc075b7fed2
Damian Alladio <damian.e.alladio@gmail.com>
2017-04-21 19:25:55 -0300

    Documentacion Casta, Asesino, Guerrero y Hechicero

--- src/main/java/dominio/Asesino.java
+++ src/main/java/dominio/Asesino.java
@@ -1,7 +1,20 @@
 package dominio;
 
+/**
+ * La clase Asesino representa el oficio de asesino del personaje
+ * y sus 3 habilidades concretas.
+ * <p>
+ */
 public class Asesino extends Casta {
 
+	/**
+	 * Crea un personaje con oficio de asesino dadas la proabilidad de
+	 * golpe crítico y de evitar daño y el daño crítico del persnaje.
+	 * <p>
+	 * @param prob_crit	es la probabilidad del personaje de dar un golpe crítico.
+	 * @param evasion	es la probabilidad del personaje de evitar el daño.
+	 * @param daño_crit	es la fuerza del daño crítico del personaje.
+	 */
 	public Asesino(double prob_crit, double evasion, double daño_crit) {
 		super(prob_crit, evasion, daño_crit);
 		this.nombreCasta="Asesino";
@@ -16,6 +29,12 @@ public class Asesino extends Casta {
 		habilidadesCasta[2] = "Robar";
 	}
 
+	/**
+	 * Define la habilidad1 para el asesino.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Golpe Crítico
 	public boolean habilidad1(Personaje caster, Peleable atacado) {
 		if (caster.getEnergia() > 10) {
@@ -26,6 +45,12 @@ public class Asesino extends Casta {
 		return false;
 	}
 
+	/**
+	 * Define la habilidad2 para el asesino.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Aumentar Evasion
 	public boolean habilidad2(Personaje caster, Peleable atacado) {
 		if (caster.getEnergia() > 10) {
@@ -39,6 +64,12 @@ public class Asesino extends Casta {
 		return false;
 	}
 
+	/**
+	 * Define la habilidad3 para el asesino.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Robar
 	public boolean habilidad3(Personaje caster, Peleable atacado) {
 		return false;
--- src/main/java/dominio/Guerrero.java
+++ src/main/java/dominio/Guerrero.java
@@ -1,7 +1,20 @@
 package dominio;
 
+/**
+ * La clase Guerrero representa el oficio de guerrero del personaje
+ * y sus 3 habilidades concretas.
+ * <p>
+ */
 public class Guerrero extends Casta {
 
+	/**
+	 * Crea un personaje con oficio de guerrero dadas la proabilidad de
+	 * golpe crítico y de evitar daño y el daño crítico del persnaje.
+	 * <p>
+	 * @param prob_crit	es la probabilidad del personaje de dar un golpe crítico.
+	 * @param evasion	es la probabilidad del personaje de evitar el daño.
+	 * @param daño_crit	es la fuerza del daño crítico del personaje.
+	 */
 	public Guerrero(double prob_crit, double evasion, double daño_crit) {
 		super(prob_crit, evasion, daño_crit);
 		this.nombreCasta = "Guerrero";
@@ -17,6 +30,12 @@ public class Guerrero extends Casta {
 		habilidadesCasta[2] = "Ignorar Defensa";
 	}
 
+	/**
+	 * Define la habilidad1 para el guerrero.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Ataque Doble
 	public boolean habilidad1(Personaje caster, Peleable atacado) { 
 		if (caster.getEnergia() > 10) {
@@ -27,6 +46,12 @@ public class Guerrero extends Casta {
 		return false;
 	}
 	
+	/**
+	 * Define la habilidad2 para el guerrero.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Aumentar Defensa
 	public boolean habilidad2(Personaje caster, Peleable atacado) { 
 		if (caster.getEnergia() > 10) {
@@ -37,6 +62,12 @@ public class Guerrero extends Casta {
 		return false;
 	}
 
+	/**
+	 * Define la habilidad3 para el guerrero.
+	 * <p>
+	 * @param caster
+	 * @param atacado
+	 */
 	// Ignorar Defensa
 	public boolean habilidad3(Personaje caster, Peleable atacado) { 
 		if (caster.getEnergia() > 10) {
