760b576c8e801315319dea8db59d671ff68cc7e1
Valeria Alejandra Couto <vale.alejandra1409@gmail.com>
2017-04-21 02:05:33 -0300

    alianza

--- src/main/java/dominio/Alianza.java
+++ src/main/java/dominio/Alianza.java
@@ -1,21 +1,23 @@
 package dominio;
 
 import java.util.LinkedList;
-
+/** La clase Alianza permite realizar vinculos con otros personajes
+ * emplea funciones que permiten agregar personajes o quitarlos 
+ */
 public class Alianza {
 
 	String nombre;
 	LinkedList<Personaje> aliados;
-/* La funcion Alianza se utiliza para determinar lazos con otros personajes*/
+/** La funcion Alianza se utiliza para determinar lazos con otros personajes*/
 	public Alianza(String nombre) {
 		this.nombre = nombre;
 		this.aliados = new LinkedList<Personaje>();
 	}
-/* Devuelve los aliados almacenados en la lista*/
+/** Devuelve los aliados almacenados en la lista*/
 	public LinkedList<Personaje> getAliados() {
 		return aliados;
 	}
-/* Esta funcion se encarga de cargar la lista con los aliados obtenidos
+/** Esta funcion se encarga de cargar la lista con los aliados obtenidos
  * 
  * @param aliados
  * 
@@ -23,14 +25,14 @@ public class Alianza {
 	public void setAliados(LinkedList<Personaje> aliados) {
 		this.aliados = aliados;
 	}
-/* Esta funcion devuelve el nombre de aliado
+/** Esta funcion devuelve el nombre de aliado
  * 
  * retorna nombre
  */
 	public String obtenerNombre() {
 		return nombre;
 	}
-/* Esta funcion se encarga de quitar personajes
+/** Esta funcion se encarga de quitar personajes
  * 
  * @param pj
  * retorna si lo elimino*/
@@ -38,7 +40,7 @@ public class Alianza {
 	public void eliminarPersonaje(Personaje pj) {
 		aliados.remove(pj);
 	}
-/* Esta funcion se encarga de agragar un personaje
+/** Esta funcion se encarga de agragar un personaje
  * 
  * @param pj
  * retorna si añado
